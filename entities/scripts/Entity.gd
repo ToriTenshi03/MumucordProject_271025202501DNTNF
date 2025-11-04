a3bcf18a3ffa4aae8b9e79dfc1428295ef1c1ab4
Nameless Tenshi <syahmiichiro03@gmail.com>
2025-11-04 22:18:24 +0800

    Creating basic Entity class and Eleonara Hikari for prototype
    Entity class will be updated into the document while Hikari Eleonara will be the playable tester for future systems

---
+++ entities/scripts/Entity.gd
@@ -0,0 +1,7 @@
+class_name  Entity
+extends CharacterBody2D
+
+@export_category("Entity Basic Stat")
+@export var entity_name:String = "EntityName"
+@export var current_hp:float = 0
+@export var max_hp:float = 100
